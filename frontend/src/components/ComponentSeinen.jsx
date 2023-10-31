291a87968534d46a9c301ca3b4491dc1bcb69141
Prathna77 <richardchum77@gmail.com>
2023-10-31 17:13:18 +0100

    done

---
+++ frontend/src/components/ComponentSeinen.jsx
@@ -0,0 +1,53 @@
+import "./ComponentSeinen.css";
+
+function ComponentSeinen() {
+  return (
+    <>
+      <div className="Categories">
+        <img
+          className="CapsuleCorp"
+          src="../src/assets/images/CapsuleCorp.png"
+          alt="CapsuleCorp logo"
+        />
+        <div>
+          <h1>Autre catégories</h1>
+        </div>
+      </div>
+      <div className="resume">
+        <img
+          className="anime"
+          src="../src/assets/images/TestGoku/97983350.png"
+          alt="sangoku"
+        />
+
+        <img
+          className="anime"
+          src="../src/assets/images/TestGoku/sayen.jpg"
+          alt="sangoku"
+        />
+        <img
+          className="anime"
+          src="../src/assets/images/TestGoku/sayen3.png"
+          alt="sangoku"
+        />
+        <img
+          className="anime"
+          src="../src/assets/images/TestGoku/instinc.jpg"
+          alt="sangoku"
+        />
+        <img
+          className="anime"
+          src="../src/assets/images/TestGoku/instinc2.jpg"
+          alt="sangoku"
+        />
+        <img
+          className="anime"
+          src="../src/assets/images/TestGoku/instinc3.jpg"
+          alt="sangoku"
+        />
+      </div>
+    </>
+  );
+}
+
+export default ComponentSeinen;
